471df01bedb89a72d58ed54e9a53f0c896dc5372
Josh Medeski <josh.medeski@gmail.com>
2022-07-06 17:01:01 -0500

    feat: get cypress working on CI

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,10 +3,8 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   e2e: {
     setupNodeEvents: (on, config) => {
-      const isDev = config.watchForFileChanges;
-      const port = process.env.PORT ?? (isDev ? "3000" : "8811");
       const configOverrides: Partial<Cypress.PluginConfigOptions> = {
-        baseUrl: `http://localhost:${port}`,
+        baseUrl: `http://localhost:3000`,
         video: !process.env.CI,
         screenshotOnRunFailure: !process.env.CI,
       };
@@ -16,7 +14,6 @@ export default defineConfig({
       on("task", {
         log: (message) => {
           console.log(message);
-
           return null;
         },
       });
