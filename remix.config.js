c284ec1879f7f6782350a64c7cf3e3fbbbb41055
Josh Medeski <josh.medeski@gmail.com>
2022-07-05 22:18:41 -0500

    fix: netlify deploy

--- remix.config.js
+++ remix.config.js
@@ -2,6 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  cacheDirectory: "./node_modules/.cache/remix",
-  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+  serverBuildTarget: "netlify",
+  server: "./server.js",
+  ignoredRouteFiles: [".*"],
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "netlify/functions/server/index.js",
+  // publicPath: "/build/",
+  // devServerPort: 8002
 };
